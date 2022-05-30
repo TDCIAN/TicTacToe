a40cec679dc632010e5a77b465b231bea04ec3a3
TDCIAN <tdcian71@gmail.com>
2022-05-31 07:41:14 +0900

    make project

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by JeongminKim on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
